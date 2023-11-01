05b395737b9d2c6e7ed009abb90cb54d9fae58cc
CodeMasterZahab <148037703+CodeMasterZahab@users.noreply.github.com>
2023-11-01 12:04:35 +0530

    Update and rename Hello.c to Addition.cpp

---
+++ Addition.cpp
@@ -0,0 +1,9 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+        clrscr():
+        int x=20 , y=20 , sum;
+        sum=x+y;
+        cout<<"\n The sum of the given number is "<<sum;
+}
